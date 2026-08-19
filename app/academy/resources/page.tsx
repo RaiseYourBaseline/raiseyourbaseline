c7718218a8fd94e3370379b75053612227ecc1ce
RaiseYourBaseline <rupal.bell@gmail.com>
2026-08-19 13:24:29 -0400

    Refactor HubSpot configuration in ResourceLibrary

--- app/academy/resources/page.tsx
+++ app/academy/resources/page.tsx
@@ -37,9 +37,7 @@ export default function AcademyResourcesPage() {
           storageKey="academy_resources_unlocked"
           pitchLine="Enter your email to unlock the Academy resource library."
           resources={academyResources}
-          hubspot={{
-            scriptSrc: "https://js-na3.hsforms.net/forms/embed/343531732.js",
-            region: "na3",
+                    hubspot={{
             formId: "5460407c-9a48-4591-9c10-7dad60b557c1",
             portalId: "343531732",
           }}
